9d6dd8ec5936a93dda1bbc4771fa7ccea182e504
jharris1679 <j_harris@live.ca>
2017-05-06 00:53:53 -0500

    csv instead of tsv that was just spaces anyway

--- parse_files.sh
+++ parse_files.sh
@@ -5,8 +5,8 @@ for gender in {M,F}; do
     for filename in $(ls names/$gender/$letter/*-stats.txt); do
       name=$(echo ${filename} | sed -n 's/names\/[A-Z]\/[A-Z]\/\(.*\)-stats.txt/\1/p')
       year=$(tail ${filename} -n +2 | cut -f1 -d,)
-      echo $name $year
-      echo $name $year >> name_ages.tsv
+      echo $name,$year
+      echo $name,$year >> name_ages.csv
     done
   done
 done
